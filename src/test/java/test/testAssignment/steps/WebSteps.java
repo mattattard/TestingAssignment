cc9ac5246aff674427bf0924f8d670b871088a6d
EzuBotz <ezek_bajada@hotmail.co.uk>
2019-01-03 16:20:42 +0100

    Added 2 scenarios

--- src/test/java/test/testAssignment/steps/WebSteps.java
+++ src/test/java/test/testAssignment/steps/WebSteps.java
@@ -3,6 +3,7 @@ package test.testAssignment.steps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -19,7 +20,7 @@ public class WebSteps {
 
     @Given("^I am a user trying to process a payment$")
     public void i_am_a_user_trying_to_process_a_payment() throws Exception{
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\matth\\OneDrive\\University\\Semester 1 - year 3\\Software Testing\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Owner\\Documents\\GitHub\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://localhost:8080/TestingAssignment");
 
@@ -39,6 +40,36 @@ public class WebSteps {
     @Then("^I should be told that the payment was successful$")
     public void i_should_be_told_that_the_payment_was_successful() throws Exception {
         //Todo: to check the process Payment was successful.
+        String expectedTest = "Succesful";
+        String actualText = driver.findElement(By.name("result")).getText();
+        Assert.assertTrue("Payment Successful",expectedTest.equals(actualText));
+        driver.quit();
+    }
+
+    @Given("^I am a user trying to process a payment$")
+    public void startPayment(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Owner\\Documents\\GitHub\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://localhost:8080/TestingAssignment");
+    }
+
+    @When("I submit a form with all data except \"([^\"]*)\" ")
+    public void submitPayment(String arg1){
+        driver.findElement(By.name("name")).sendKeys("Matthew Attard");
+        driver.findElement(By.name("address")).sendKeys("jksdjkgj");
+        driver.findElement(By.name("card")).sendKeys("378282246310005");
+        driver.findElement(By.name("expiry")).sendKeys("");
+        driver.findElement(By.name("cvv")).sendKeys("745");
+        driver.findElement(By.name("amount")).sendKeys("15320");
+
+        driver.findElement(By.name(arg1));
+    }
+
+    @Then("I should be told that \"([^\"]*)\" is required")
+    public void unsuccessfulPayment(String args1){
+        String expectedTest = args1 + "is missing";
+        String actualText = driver.findElement(By.name("submitResult")).getText();
+        Assert.assertTrue("Payment Successful",expectedTest.equals(actualText));
         driver.quit();
     }
 }
--- src/test/java/web.feature
+++ src/test/java/web.feature
@@ -1,10 +1,24 @@
 
-  Feature: Web Function:
-    In order to not learn math
-    As someone who is bad at math
-    I want to be able to add numbers using a Calculator
+  Feature: Payment Processing Function:
+    In order to process payment
+    As someone who is wanting to pay
+    I want to be able to process payment securely.
 
     Scenario:  All details entered correctly:
       Given     I am a user trying to process a payment
       When      I submit correct details
       Then      I should be told that the payment was successful
+
+
+    Scenario Outline:  Any Field is left empty
+      Given     I am a user trying to process a payment
+      When      I submit a form with all data except <fieldname>
+      Then      I should be told that <fieldname> is required
+
+      Examples:
+      |fieldname|
+      | name |
+      | address |
+      | card |
+      | expiry |
+      | cvv |
